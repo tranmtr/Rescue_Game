21d03f53911f13ffa25b655952779aef63505f4f
Duong Thai Tran <duongthaitran68@gmail.com>
2024-03-15 09:45:45 +0700

    phat hien va cham bang hinh chu nhat

--- Game_funny/main.cpp
+++ Game_funny/main.cpp
@@ -105,8 +105,8 @@ class Dot
 		//Takes key presses and adjusts the dot's velocity
 		void handleEvent( SDL_Event& e );
 
-		//Moves the dot
-		void move();
+		//Moves the dot and checks collision
+		void move( SDL_Rect& wall );
 
 		//Shows the dot on the screen
 		void render();
@@ -117,6 +117,9 @@ class Dot
 
 		//The velocity of the dot
 		int mVelX, mVelY;
+
+		//Dot's collision box
+		SDL_Rect mCollider;
 };
 
 
@@ -129,6 +132,9 @@ bool loadMedia();
 
 //Frees media and shuts down SDL
 void close();
+
+//Box collision detector
+bool checkCollision( SDL_Rect a, SDL_Rect b );
 
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
@@ -259,6 +265,10 @@ Dot::Dot()
     mPosX = 0;
     mPosY = 0;
 
+    //Set collision box dimension
+	mCollider.w = DOT_WIDTH;
+	mCollider.h = DOT_HEIGHT;
+
     //Initialize the velocity
     mVelX = 0;
     mVelY = 0;
@@ -298,26 +308,30 @@ void Dot::handleEvent( SDL_Event& e )
     }
 }
 
-void Dot::move()
+void Dot::move( SDL_Rect& wall )
 {
     //Move the dot left or right
     mPosX += mVelX;
+    mCollider.x = mPosX;
 
     //If the dot went too far to the left or right
-    if( ( mPosX < 0 ) || ( mPosX + DOT_WIDTH > SCREEN_WIDTH ) )
+    if( ( mPosX < 0 ) || ( mPosX + DOT_WIDTH > SCREEN_WIDTH ) || checkCollision( mCollider, wall ) )
     {
         //Move back
         mPosX -= mVelX;
+        mCollider.x = mPosX;
     }
 
     //Move the dot up or down
     mPosY += mVelY;
+    mCollider.y = mPosY;
 
-    //If the dot went too far up or down
-    if( ( mPosY < 0 ) || ( mPosY + DOT_HEIGHT > SCREEN_HEIGHT ) )
+    //If the dot collided or went too far up or down
+    if( ( mPosY < 0 ) || ( mPosY + DOT_HEIGHT > SCREEN_HEIGHT ) || checkCollision( mCollider, wall ) )
     {
         //Move back
         mPosY -= mVelY;
+        mCollider.y = mPosY;
     }
 }
 
@@ -459,7 +473,50 @@ void close()
 	SDL_Quit();
 }
 
+bool checkCollision( SDL_Rect a, SDL_Rect b )
+{
+    //The sides of the rectangles
+    int leftA, leftB;
+    int rightA, rightB;
+    int topA, topB;
+    int bottomA, bottomB;
 
+    //Calculate the sides of rect A
+    leftA = a.x;
+    rightA = a.x + a.w;
+    topA = a.y;
+    bottomA = a.y + a.h;
+
+    //Calculate the sides of rect B
+    leftB = b.x;
+    rightB = b.x + b.w;
+    topB = b.y;
+    bottomB = b.y + b.h;
+
+    //If any of the sides from A are outside of B
+    if( bottomA <= topB )
+    {
+        return false;
+    }
+
+    if( topA >= bottomB )
+    {
+        return false;
+    }
+
+    if( rightA <= leftB )
+    {
+        return false;
+    }
+
+    if( leftA >= rightB )
+    {
+        return false;
+    }
+
+    //If none of the sides from A are outside B
+    return true;
+}
 
 int main( int argc, char* args[] )
 {
@@ -492,6 +549,13 @@ int main( int argc, char* args[] )
 			//Current animation frame
 			//int frame = 0;
 
+            //Set the wall
+			SDL_Rect wall;
+			wall.x = 300;
+			wall.y = 40;
+			wall.w = 40;
+			wall.h = 250;
+
 			//While application is running
 			while( !quit )
 			{
@@ -508,19 +572,22 @@ int main( int argc, char* args[] )
 
 				}
 				//Move the dot
-				dot.move();
+				dot.move( wall );
 
 				//Clear screen
 				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 				SDL_RenderClear( gRenderer );
 
 				//Render current frame
-				//SDL_Rect* currentClip = &gSpriteClips[ frame / 4 ]; // De o ngoai khong the cho hiep si don chay duoc, chua ro li do
-				//gSpriteSheetTexture.render( ( SCREEN_WIDTH - currentClip->w ) / 2, ( SCREEN_HEIGHT - currentClip->h ) / 2, &gSpriteClips[ frame / 4 ], NULL, NULL, flipType );
+				//SDL_Rect* currentClip = &gSpriteClips[ frame / 2 ]; // De o ngoai khong the cho hiep si don chay duoc, chua ro li do
+				//gSpriteSheetTexture.render( ( SCREEN_WIDTH - currentClip->w ) / 2, ( SCREEN_HEIGHT - currentClip->h ) / 2, &gSpriteClips[ frame / 2 ], NULL, NULL, flipType );
+
+                //Render wall
+				SDL_SetRenderDrawColor( gRenderer, 0x00, 0x00, 0x00, 0xFF );
+				SDL_RenderDrawRect( gRenderer, &wall );
 
                 //Render objects
 				dot.render();
-
 				//Update screen
 				SDL_RenderPresent( gRenderer );
 
